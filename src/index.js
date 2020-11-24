a0492f8e6bbd149ff2f785b7016ce0af495e15d8
이홍기 <ihong-gi@iHong-gis-2nd-Mac.local>
2020-11-25 00:06:00 +0900

    redux-counter little iprovement

--- src/index.js
+++ src/index.js
@@ -4,25 +4,28 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+number.innerHTML = 0
+
+const ADD = "ADD";
+const MINUS = "MINUS";
+
 const countModifier = (count = 0, action) => {
-  if(action.type === "ADD") {
-    return count + 1
-  } else if (action.type === "MINUS"){
-    return count - 1
-  } else {
-    return count
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
 }
 
-number.innerHTML = 0
 
-const countStore = createStore(countModifier)
+const countStore = createStore(countModifier);
 
-const onChange = () => {
-  number.innerHTML = countStore.getState()
-}
+const onChange = () => { number.innerHTML = countStore.getState() };
 
-countStore.subscribe(onChange)
+countStore.subscribe(onChange);
 
-add.addEventListener("click", () => {countStore.dispatch({type: "ADD"})})
-minus.addEventListener("click", () => {countStore.dispatch({type: "MINUS"})})
+add.addEventListener("click", () => {countStore.dispatch({type: ADD})});
+minus.addEventListener("click", () => {countStore.dispatch({type: MINUS})});
