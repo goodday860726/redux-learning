a00b3004e82701363187134f996432ae663cec05
이홍기 <ihong-gi@iHong-gis-2nd-Mac.local>
2020-11-29 11:08:58 +0900

    Redux counter

--- src/index.js
+++ src/index.js
@@ -1,31 +1,74 @@
-import {createStore} from "redux";
+import { createStore } from "redux";
 
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-number.innerHTML = 0
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
 
-const ADD = "ADD";
-const MINUS = "MINUS";
+const addTodo = text => {
+  return {
+    type: ADD_TODO,
+    text
+  }
+};
 
-const countModifier = (count = 0, action) => {
-  switch (action.type) {
-    case ADD:
-      return count + 1;
-    case MINUS:
-      return count - 1;
-    default:
-      return count;
+const deleteTodo = id => {
+  return {
+    type: DELETE_TODO,
+    id
   }
 }
 
+const reducer = (state = [], action) => {
+  switch(action.type) {
+    case ADD_TODO:
+      return [ {text: action.text, id: Date.now()}, ...state ];
+    case DELETE_TODO:
+      return state.filter(todo => todo.id !== action.id);
+    default:
+      return state;
+  }
+}
 
-const countStore = createStore(countModifier);
+const store = createStore(reducer);
 
-const onChange = () => { number.innerHTML = countStore.getState() };
+store.subscribe(() => {console.log(store.getState())});
 
-countStore.subscribe(onChange);
+const dispatchAddTodo = (text) => {
+  store.dispatch(addTodo(text))
+};
 
-add.addEventListener("click", () => {countStore.dispatch({type: ADD})});
-minus.addEventListener("click", () => {countStore.dispatch({type: MINUS})});
+const dispatchDeleteTodo = e => {
+  const id = parseInt(e.target.parentNode.id);
+  store.dispatch(deleteTodo(id));
+}
+
+const printTodos = () => {
+  const todos = store.getState();
+  ul.innerHTML = "";
+  todos.forEach(todo => {
+    const li = document.createElement("li");
+    const btn = document.createElement("button");
+    li.id = todo.id;
+    li.innerHTML = todo.text;
+    btn.innerHTML = "DEL";
+    btn.addEventListener("click", dispatchDeleteTodo);
+    li.appendChild(btn);
+    ul.appendChild(li);
+  });
+}
+
+store.subscribe(printTodos);
+
+
+const onSubmit = e => {
+  e.preventDefault();
+  const todo = input.value;
+  input.value = "";
+  dispatchAddTodo(todo)
+};
+
+
+form.addEventListener("submit", onSubmit);
